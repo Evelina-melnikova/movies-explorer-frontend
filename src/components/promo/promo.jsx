8b3d733b75e31f904ea37cb97a3360853396cc0d
Evelina Melnikova <melnikovaa45@yandex.ru>
2024-03-07 00:28:18 +0300

    ревью_2

--- src/components/promo/promo.jsx
+++ src/components/promo/promo.jsx
@@ -3,16 +3,18 @@ import './promo.css';
 
 function Promo() {
     return (
-        <nav className='promo'>
+        <section className='promo'>
             <div className='promo__wrapper'>
                 <h1 className='title'> Учебный проект студента факультета Веб&#8209;разработки. </h1>
+                <nav>
                 <ul className='promo__link-container'>
-                <li><a className='promo__link' href='#about-project'> О проекте </a></li>
-                <li><a className='promo__link' href='#techs'> Технологии </a></li>
-                <li><a className='promo__link' href='#about-me'> Студент </a></li>
+                    <li><a className='promo__link' href='#about-project'> О проекте </a></li>
+                    <li><a className='promo__link' href='#techs'> Технологии </a></li>
+                    <li><a className='promo__link' href='#about-me'> Студент </a></li>
                 </ul>
+                </nav>
             </div>
-        </nav>
+        </section>
     );
 }
 
